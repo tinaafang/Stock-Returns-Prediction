915878deec37833519f2e228a87b2999044e464f
tinaafang <94018300+tinaafang@users.noreply.github.com>
2022-01-04 14:04:30 -0500

    Update

--- back_end.py
+++ back_end.py
@@ -13,56 +13,71 @@ from sklearn.model_selection import GridSearchCV
 import pickle
 
 def get_tickers():
+    '''Get a list of tickers listed in US market'''
+    # Get lists of tickers and put them in a set
     sp500 = set(si.tickers_sp500())
     nasdaq = set(si.tickers_nasdaq())
     dow = set(si.tickers_dow())
     other = set(si.tickers_other())
-
     total = set.union(sp500,nasdaq,dow,other)
-    len(total)
 
+    # Find out the undesired tickers(such as tickers with warrants or rights)
     filter_lst = ['W','R','P','Q','U','T','L','Z']
     bad_tickers = set()
     for i in total:
         if (len(i) >= 5 and i[-1] in filter_lst) or '$' in i or len(i) == 0:
             bad_tickers.add(i)
-            
+    
+    # Subtract the undesirable tickers from the ticker set
     good_tickers = total - bad_tickers
+    # Return the remaining tickers in a list
     return list(good_tickers)
 
-def timing(ticker,today):
+def timing(ticker,today,tomorrow):
+    '''Find the dates that works for calculating returns'''
     try:
+        # Returns as features are returns from start date to mid date, which are what we're given
+        # Returns as target are returns from mid date to end date, which are what we're trying to predict
+
+        # End date is the latest date with valid stock data, the closing price on end date serves as the end date of calculating the target returns
         end_date = ticker.history(start = today -  datetime.timedelta(10),end = today,interval = '1d')['Close'].index[-1]
-        end_end = today
+        end_end = tomorrow
+        # Mid date is 90 days before the end date, the closing price on mid date serves as the end date of calculating the features returns and the start date of calculating the target returns
         mid_date = end_date -  datetime.timedelta(90)
         mid_end = mid_date +  datetime.timedelta(10)
+        # Start date is 90 days before the mid date, the closing price on start date serves as the start date of calculating the features returns
         start_date = mid_date -  datetime.timedelta(90)
         start_end = start_date +  datetime.timedelta(10)
+        # Return the dates in string
         return str(start_date)[:10],str(start_end)[:10],str(mid_date)[:10],str(mid_end)[:10],str(end_date)[:10],str(end_end)[:10]
     except:
         pass
     
-def fill(symbol):
+def prepare_features(symbol):
+    '''prepare the information needed for calculating the feature columns'''
+    # Set up today's date and the ticker
     today = date.today()
     ticker = yf.Ticker(symbol)
     try:
+        # Get start price,mid price and end price using the dates from the timing function
         start_date,start_end,mid_date,mid_end,end_date,end_end = timing(ticker,today)
         end_price = float(ticker.history(start = end_date,end = end_end,interval = '1d')['Close'][0])
         start_price = float(ticker.history(start = start_date,end = start_end,interval = '1d')['Close'][0])
         mid_price = float(ticker.history(start = mid_date,end = mid_end,interval = '1d')['Close'][0])
+        # Get the dividend yield, average volume and fifty-day average 
         dividendYield = ticker.info['dividendYield']
         averageVolume = ticker.info['averageVolume']
         fiftyDayAverage = ticker.info['fiftyDayAverage']
-        #currentPrice = ticker.info['currentPrice']
-        
+        # Return the above information
         return [symbol,averageVolume,fiftyDayAverage,dividendYield,start_price,mid_price,end_price]
     except:
         pass
 
 
 def collect_data(df, symbols):
+    '''Use multithreading to get information from the prepare_features function'''
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        result = executor.map(fill,symbols)
+        result = executor.map(prepare_features,symbols)
     for i in result:
         try:
             df[i[0]] = [i[1],i[2],i[3],i[4],i[5],i[6]]
@@ -70,80 +85,101 @@ def collect_data(df, symbols):
             pass
     return df
 
-def get_features(features):
-    features = features.T
-    features.columns = ['averageVolume','fiftyDayAverage','dividendYield','start_price','mid_price','end_price']
-    features['dividendYield']=features['dividendYield'].fillna(0)
-    features['dividend']=features['dividendYield']*features['mid_price']/365*90
-    features = features.dropna()
-    features['shares'] = 100000/features['start_price']
-    features['returns'] = (features['shares']*(features['mid_price']-features['start_price']))/100000*100
-    features['target_shares'] = 100000/features['mid_price']
-    features['target_returns'] = (features['target_shares']*(features['end_price']-features['mid_price']))/100000*100
-    features['diff'] = features['mid_price'] - features['fiftyDayAverage']
-    target = (features['target_returns']>0) 
+def get_dataset(data):
+    '''Create the dataset using the information collected before'''
+    # Transpose the dataframe
+    data = data.T
+    # Rename the columns
+    data.columns = ['averageVolume','fiftyDayAverage','dividendYield','start_price','mid_price','end_price']
+    # Replace Nan in dividendYield with 0
+    data['dividendYield']=data['dividendYield'].fillna(0)
+    # Calculate the dividend for feature returns and dividend returns
+    data['feature_dividend']=data['dividendYield']*data['mid_price']/365*90
+    data['target_dividend']=data['dividendYield']*data['end_price']/365*90
+    # Drop any rows with Nan
+    data = data.dropna()
+    # Calculate the feature and target returns by adding the capital gains and the dividend
+    data['feature_returns'] = (data['mid_price']-data['start_price']+data['feature_dividend'])
+    data['target_returns'] = (data['end_price']-data['mid_price']+data['target_dividend'])
+    # Diff measures how much the mid price is above or below the average price
+    data['diff'] = data['mid_price'] - data['fiftyDayAverage']
+    # The target is 1 if the target return is positive, 0 if the target return is negative
+    target = (data['target_returns']>0) 
     target = target.values.tolist()
     mapping = {True: 1, False: 0}
-    replace = mapping.get
-    target = [replace(n, n) for n in target]
-    features['target'] = target
-    features.reset_index(drop = True,inplace=True)
-    features = features.drop(['start_price','end_price','dividendYield','fiftyDayAverage','shares','target_shares','target_returns'],axis=1)
-    return features
+    target = [mapping.get(n, n) for n in target]
+    data['target'] = target
+    # Reset the index and drop the extra columns
+    data.reset_index(drop = True,inplace=True)
+    data = data.drop(['start_price','end_price','dividendYield','fiftyDayAverage','target_returns'],axis=1)
+    # Return the dataset
+    return data
 
 
 def get_x_y(data):
+    '''Get the features and the target(x and y), as well as the mins and maxs used to normalize the features'''
+    # Drop the target, the remaining columns are features
     features = data.drop(['target'],axis = 1)
+    # Get the mins and maxs for each column in lists
     mins = features.apply(lambda x: x.min(axis=0))
     maxs = features.apply(lambda x: x.max(axis=0))
     mins = mins.values.tolist()
     maxs = maxs.values.tolist()
+    # Normalize the features to values between 0 and 1
     features = features.apply(lambda x: (x - x.min(axis=0))/ (x.max(axis=0) - x.min(axis= 0 )))
+    # Convert features and target into numpy arrays
     y = np.array(data.target)
     x = np.array(list(zip(features['averageVolume'].tolist(),features['mid_price'].tolist(),features['returns'].tolist(),features['diff'].tolist(),features['dividend'].tolist())))
+    # Return features, target and the mins and maxs of each column
     return x,y, [mins,maxs]
 
 
 def main():
-    features = pd.DataFrame()
+    '''Generate the dataset and the machine learning model used to predict the stock returns'''
+    # Create an empty dataframe
+    data = pd.DataFrame()
+    # Get the tickers, collect data needed, then calculate the features and target
     symbols = get_tickers()
-    features = collect_data(features, symbols)
-    features = get_features(features)
-    
-    features.to_csv(r'C:\Users\sunny\Desktop\side\static\my_data.csv',index=False)
+    data = collect_data(data, symbols)
+    data = get_dataset(data)
+    # Store the dataset
+    data.to_csv(r'C:\Users\sunny\Desktop\side\static\my_data.csv',index=False)
     data = pd.read_csv(r'C:\Users\sunny\Desktop\side\static\my_data.csv')
-    
+    # Get features, target and split them into train set and test set. Also store the mins and maxs.
     x,y,mins_and_maxs = get_x_y(data)
     x_train,x_test,y_train,y_test = sklearn.model_selection.train_test_split(x,y,test_size = 0.1)
-    
-    
+    with open(r"C:\Users\sunny\Desktop\side\static\mins_and_maxs.pickle",'wb') as f:
+        pickle.dump(mins_and_maxs,f)
+    # Create a parameter grid for grid search
     param_grid = [{
     'kernel' :['linear', 'poly', 'rbf', 'sigmoid'],
     'gamma':['auto', 1, 0.1, 0.01, 0.001, 0.0001],
     'C': [0.1,1, 10, 100,500],
     'degree':[2,3,4,5,6,7,8]}]
-
+    # Search the best model with parameters from the parameter grid and train the model
     model = GridSearchCV(svm.SVC(), param_grid = param_grid, refit=True,verbose=2)
     best_model= model.fit(x_train,y_train)
+    # Initiaize the best accuracy to 0
     best_acc = 0
-    
+    # Iterate to find a higher accuracy
     for i in range(1000):
+        # Split the dataset, train the model, get the prediction then score the accuracy
         x_train,x_test,y_train,y_test = sklearn.model_selection.train_test_split(x,y,test_size = 0.1)
         model = best_model.best_estimator_
         model.fit(x_train,y_train)
         y_predict =model.predict(x_test)
         acc = metrics.accuracy_score(y_test,y_predict)
+        # If the current acc is greater then the best acc, replace best_acc with acc then store the model
         if acc>best_acc:
             best_acc = acc
             with open(r"C:\Users\sunny\Desktop\side\static\model.pickle",'wb') as f:
                 pickle.dump(model,f)
+    # Store the best accuracy
     with open(r"C:\Users\sunny\Desktop\side\static\acc.pickle",'wb') as f:
         pickle.dump(best_acc,f)
-    with open(r"C:\Users\sunny\Desktop\side\static\mins_and_maxs.pickle",'wb') as f:
-        pickle.dump(mins_and_maxs,f)
-    print('done!',best_acc)
-
-schedule.every().wednesday.at('12:13').do(main)
+    
+# Run the main function every monday at 00:00
+schedule.every().monday.at('00:00').do(main)
 while 1:
     schedule.run_pending()
     time.sleep(1)
--- main.py
+++ main.py
@@ -2,58 +2,186 @@ from flask import Flask,render_template,request,flash,redirect,url_for,session
 import yfinance as yf
 import numpy as np
 import datetime
-from datetime import date
+from datetime import datetime,date, timedelta
 import pickle
+import matplotlib.pyplot as plt
+import pandas as pd
 
+# Create an app and set up the secret key for sessions
 app = Flask(__name__)
 app.secret_key= 'hello'
 
 def get_ticker_info(ticker):
+    '''Collect the information needed to predict the ticker(collect the features)'''
     try:
+        # Call the ticker
         ticker = yf.Ticker(ticker)
+        # Get the average volume and mid price from the info dictionary
         averageVolume = ticker.info['averageVolume']
-        mid_price = ticker.info['currentPrice']
+        current_price = ticker.info['currentPrice']
+        average = ticker.info['fiftyDayAverage']
+        diff = current_price - average
+        # Get the closing price 90 days ago
         today= date.today()
-        start_date = str(today -  datetime.timedelta(90))[:10]
-        end_date = str(today -  datetime.timedelta(85))[:10]
+        start_date = str(today -  timedelta(90))[:10]
+        end_date = str(today -  timedelta(85))[:10]
         start_price = float(ticker.history(start = start_date, end = end_date, interval = '1d' )['Close'][0])
+        # Calculate the dividend, if the output is None, dividend is 0
         try:
-            dividend = mid_price*ticker.info['dividendYield']/365*90
+            dividend = current_price*ticker.info['dividendYield']/365*90
         except:
             dividend = 0
-        average = ticker.info['fiftyDayAverage']
-        diff = mid_price - average
-        shares = 100000 / mid_price
-        returns= shares*(mid_price-start_price+dividend)/100000*100
-        
-        return np.array([[averageVolume,mid_price,dividend,returns,diff]])
+        # Calculate the returns: capital gains + dividend
+        returns= (current_price-start_price+dividend)
+        # Return the features in list in array
+        return np.array([[averageVolume,current_price,dividend,returns,diff]])
     except:
+        # If any error occured, do nothing
         pass
 
 def normalize(lst):
+    '''Normalize the features values between 0 to 1'''
+    # Get the mins and maxs of the features columns
     minmax_in = open(r"C:\Users\sunny\Desktop\side\static\mins_and_maxs.pickle",'rb')
     mins_and_maxs = pickle.load(minmax_in)
     mins = mins_and_maxs[0]
     maxs = mins_and_maxs[1]
+    # Using the mins and maxs to normalize the features
     for i in range(len(lst)):
         lst[i] = (lst[i]-mins[i])/ (maxs[i] - mins[i])
     return lst
 
 def predict_stock(ticker_info):
+    '''Given the features, predict the stock returns'''
+    # Get the model and accuracy we stored
     model_in = open(r"C:\Users\sunny\Desktop\side\static\model.pickle",'rb')
     acc_in = open(r"C:\Users\sunny\Desktop\side\static\acc.pickle",'rb')
     acc = pickle.load(acc_in)
     model = pickle.load(model_in)
+    # Predict the returns
     returns_prediction = model.predict(ticker_info)
+    # Return the prediction and the accuracy
     result_lst = [returns_prediction, acc]
     return result_lst
 
+def get_dates():
+    '''Get valid dates for extracting the 1 day and the 1 year closing price'''
+    '''Note that the stock market opens at 9:30 am, close at 4:00 pm, also closes on weekends  '''
+    # Get today's date
+    today = date.today() 
+    # Store the opening hour of the stock market
+    morning = datetime.now().replace(hour=9, minute=30, second=0, microsecond=0)
+    # If today is Saturday or Sunday, use Friday's data
+    if today.weekday() == 5:
+        start_date1d = today - timedelta(1)
+    elif today.weekday() == 6:
+        start_date1d = today - timedelta(2)
+    else: 
+        # Otherwise, use the data on the current date
+        start_date1d = today
+        # If it is before 9:30 am, use the data from yesterday
+        if datetime.now()<morning:
+            start_date1d -= timedelta(1)
+            # If it is Monday and is before 9:30 am, use data from the last Friday
+            if today.weekday() == 0:
+                start_date1d -= timedelta(2)
 
-@app.route('/')
-@app.route('/home')
-def home():
-    return render_template("index.html")
+    # Get the date 365 days ago for extracting the 1 year closing price
+    start_date1y = today -  timedelta(365)
+    # Get tomorrow's date
+    tomorrow = today +  timedelta(1)
+    # Get the dates in 'yyyy-mm-dd' format
+    start_date1y = str(start_date1y)[:10]
+    tomorrow = str(tomorrow)[:10]
+    return start_date1y,start_date1d, tomorrow
 
+
+def additional_info(symbol):
+    '''Get extra information about the stock, including '''
+    '''current price,currency,52 week high,52 week low,P/E ratio,dividend yield,marketCap,open,high,low'''
+    # Call the ticker
+    ticker = yf.Ticker(symbol)
+    # Get valid dates from the get_dates function
+    start_date1y,start_date1d, tomorrow = get_dates()
+    # Get the 1 year closing price and the average 1 year closing price
+    prices1y = ticker.history(start = start_date1y, end = tomorrow, interval = '1d')['Close']
+    avg = prices1y.mean()
+    # Try to get the 1 day price in 5-min interval and 1d interval
+    try:
+        start_date1d_str = str(start_date1d)[:10]
+        prices5m = ticker.history(start = start_date1d_str, end = tomorrow, interval = '5m')['Open']
+        prices1d = ticker.history(start = start_date1d_str, end = tomorrow, interval = '1d')
+    # If any error occurs(e.g. the stock market is in holiday), try again using the previous date
+    except:
+        start_date1d_str = str(start_date1d - timedelta(1))[:10]
+        prices5m = ticker.history(start = start_date1d_str, end = tomorrow, interval = '5m')['Open']
+        prices1d = ticker.history(start = start_date1d_str, end = tomorrow, interval = '1d')
+    
+    # Convert the index of prices5m in string 'hh:mm'
+    time = prices5m.index.tolist()
+    time = [str(i)[11:16] for i in time]
+    prices5m.index = time
+
+    # Get the open,.high, low
+    open = round(prices1d['Open'][0],2)
+    high = round(prices1d['High'][0],2)
+    low = round(prices1d['Low'][0],2)
+    # 
+    prev_close = prices1y[-2]
+    current = ticker.info['currentPrice']
+    currency = ticker.info['currency']
+    high52 = ticker.info['fiftyTwoWeekHigh']
+    low52 = ticker.info['fiftyTwoWeekLow']
+    marketCap = ticker.info['marketCap']
+    unit = ''
+    if marketCap > 1000000000000:
+        marketCap = marketCap/1000000000000
+        unit = 'T'
+    elif marketCap > 1000000000:
+        marketCap = marketCap/1000000000
+        unit = 'B'
+    elif marketCap > 1000000000:
+        marketCap = marketCap/1000000
+        unit = 'M'
+    marketCap = str(round(marketCap,2))+unit
+    
+    try:
+        pe = round(ticker.info['trailingPE'],2)
+    except:
+        pe = '-'
+    
+    try:
+        dividendYield = ticker.info['dividendYield']*100
+        if dividendYield is None or dividendYield == 'None':
+            dividendYield = '-'
+    except:
+        dividendYield = 0
+
+    fig = plt.figure(figsize = (6,4))
+    ax = fig.add_subplot()
+    ax.plot(prices1y.index, prices1y,label = 'Closing Prices')
+    ax.axhline(y = avg, color = 'black',linestyle = 'dashed',label = 'Average: '+str(round(avg,2)))
+    ax.set_title(symbol+": Historical Closing Price 1Y")
+    ax.legend(loc = 'best')
+    fig.savefig(r'C:\Users\sunny\Desktop\side\static\price1y.png')
+    fig.clear()
+    
+    fig1 = plt.figure(figsize = (6,4))
+    ax1 = fig1.add_subplot()
+    ax1.plot(prices5m.index, prices5m,label = 'Closing Prices')
+    ax1.axhline(y = prev_close, color = 'black',linestyle = 'dashed',label = 'Previous Close: '+str(round(prev_close,2)))
+    ax1.set_title(symbol+": Closing Prices on "+start_date1d_str)
+    ax1.legend(loc = 'best')
+    ax1.margins(x = 0.05)
+    ax1.set_xticks(ax1.get_xticks()[::12])
+    fig1.savefig(r'C:\Users\sunny\Desktop\side\static\price1d.png')
+    fig1.clear()
+
+    return current,currency,high52,low52,pe,dividendYield,marketCap,open,high,low
+
+
+
+@app.route('/',methods = ['POST','GET'])        
 @app.route('/prediction',methods = ['POST','GET'])
 def prediction():
     if request.method == 'POST':
@@ -62,7 +190,7 @@ def prediction():
         if ticker:
             return redirect(url_for("result", ticker = ticker))
         else:
-            flash("Please enter a proper ticker.",'info')
+            flash("Please enter a proper ticker.",'warning')
             return redirect(url_for("prediction"))
     else:
         return render_template("prediction.html")
@@ -73,7 +201,8 @@ def result(ticker):
         return redirect(url_for("prediction"))
     if ticker in session:
         result_lst = session[ticker]
-        return render_template("result.html",ticker = result_lst[0], final_result = result_lst[1], acc = result_lst[2]*100)
+        info_lst = additional_info(ticker)
+        return render_template("result.html",info_lst = info_lst,ticker = result_lst[0], final_result = result_lst[1], acc = result_lst[2]*100)
     else:
         ticker_info = get_ticker_info(ticker)
         test = np.sum(ticker_info)
@@ -86,14 +215,16 @@ def result(ticker):
                 final_result = 'positive'
             else:
                 final_result = 'negative'
+            info_lst = additional_info(ticker)
             session[ticker] = [ticker,final_result,acc]
             result_lst = session[ticker]
-            return render_template("result.html",ticker = result_lst[0], final_result = result_lst[1], acc = result_lst[2]*100)
+            return render_template("result.html",info_lst = info_lst, ticker = result_lst[0], final_result = result_lst[1], acc = result_lst[2]*100)
         else:
-            flash(f"Can't get info for {ticker}, please try another ticker. ",'info')
+            flash(f"Can't get info for {ticker}, please try another ticker. ",'warning')
             return redirect(url_for("prediction"))
 
-    
+
+
 
 
 if __name__ == '__main__':
